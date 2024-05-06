67060081b0498e034eb585a5d92017cb1d360798
Isaac Leandro Luna Araujo <isaac6126@DIGITALCOLLEGE.LOCAL>
2024-05-06 20:33:54 -0300

    ajeitado bug do script

--- scripst/index.js
+++ scripst/index.js
@@ -1,17 +1,17 @@
 
 function convertTemperature() {
-    const celsiusInout = document.getElementById("celsius").value;
-    const fahrenheitInout = document.getElementById("fahrenheit").value;
+    const celsiusInput = document.getElementById("celsius").value;
+    const fahrenheitInput = document.getElementById("fahrenheit").value;
 
-    if (celsiusInout.value !== "") {
-        const celsiusValue = parseFloat(celsiusInout);
+    if (celsiusInput.value !== "") {
+        const celsiusValue = parseFloat(celsiusInput);
         const fahrenheitValue = celsiusValue * 1.8 + 32;
 
-        fahrenheitInout.value = fahrenheitValue.toFixed(2);
-    } else if (fahrenheitInout.value !== "") {
-        const fahrenheitValue = parseFloat(fahrenheitInout);
+        fahrenheitInput.value = fahrenheitValue.toFixed(2);
+    } else if (fahrenheitInput.value !== "") {
+        const fahrenheitValue = parseFloat(fahrenheitInput);
         const celsiusValue = (fahrenheitValue - 32) / 1.8;
 
-        celsiusInout.value = celsiusValue.toFixed(2);
+        celsiusInput.value = celsiusValue.toFixed(2);
     }
 }
